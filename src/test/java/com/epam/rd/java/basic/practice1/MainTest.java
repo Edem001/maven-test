4bcce70a7a73cf165ea665f2bb2960e55c99e103
qdMhWZLB <qdmhwzlb@email.com>
2021-09-20 13:00:22 +0300

    Initial commit

---
+++ src/test/java/com/epam/rd/java/basic/practice1/MainTest.java
@@ -0,0 +1,30 @@
+package com.epam.rd.java.basic.practice1;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testHelloWorld(){
+        Main.main(null);
+        assertEquals("Hello World\r\n", outContent.toString());
+    }
+}
